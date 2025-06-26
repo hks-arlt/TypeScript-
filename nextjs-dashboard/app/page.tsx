424dea9798c91aaa877790b00de1582019170e6a
Mayra <87337112+DDHGDBNHDUEU@users.noreply.github.com>
2025-06-26 16:38:44 -0300

    colores

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,3 +1,4 @@
+import '@/app/ui/global.css';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
@@ -10,10 +11,13 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>HOLA MUNDO</strong> 
+          <p className={`text-xl text-lime-400 md:text-3xl md:leading-normal`}>
+            <strong>HOLA MUNDO</strong>
           </p>
-
+           <p className="text-lime-400">
+          Estoy practicando typescrip
+           </p>                                       
+   
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
